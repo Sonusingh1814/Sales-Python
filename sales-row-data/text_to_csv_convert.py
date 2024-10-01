fabfb30fea69694a7e84df7c23cf09d405e9ebff
Sonusingh1814 <103406130+Sonusingh1814@users.noreply.github.com>
2024-10-01 21:24:41 +0530

    Add files via upload

---
+++ sales-row-data/text_to_csv_convert.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+
+import pandas as pd
+
+# Read data from a text file
+df = pd.read_csv('sales-december-2019.txt', delimiter=',')  # Ensure delimiter matches your data format
+
+# Save to CSV file
+df.to_csv('sales-december-2019.csv', index=False)
+
+print("sales-december-2019.csv")
